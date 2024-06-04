c2dd6ad34aa2cef92646b781eb5e192dc221a6c4
cTu4 <amedvedev@keng.ru>
2024-06-04 17:16:01 +0300

    test deploy heroku

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-shop/",
   plugins: [
     vue(),
     VueDevTools(),
